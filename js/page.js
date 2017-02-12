fe2d4d4458476fe79b82fdd49dd7b959b752ce72
eternalless <244041147@qq.com>
2017-02-12 11:01:22 +0800

    first

---
+++ js/page.js
@@ -0,0 +1,67 @@
+var page_content = [
+'<div class="default"><img alt="" src="http://cn.yimg.com/xx1/2012032317.jpg" height="441" width="277"></a><p align="center"></a></div>',
+'<div class="default"><img src="http://cn.yimg.com/2012323huofa/taojiefm2.jpg" style="cursor:pointer;" onclick="$(\'#book .arrow .ar\').click();"/></div>',
+
+'<p class="title">懵懵懂懂约会</p>' +
+'<p class="p1"><img align="left" class="i_1" alt="" src="http://cn.yimg.com/sports/img/news/life/20101109/icon_life.gif"/> ' +
+'<img alt="" src="images/001.jpg" ></p>',
+
+    '<p class="title">阳光午后</p>' +
+    '<p class="p1"><img alt="" src="images/002.jpg" ></p>',
+
+    '<p class="title">牵手漫步</p>' +
+    '<p class="p1"><img alt="" src="images/003.jpg" ></p>',
+
+    '<p class="title">寸金草坪</p>' +
+    '<p class="p1"><img alt="" src="images/004.jpg" ></p>',
+
+    '<p class="title">甜蜜金沙湾</p>' +
+    '<p class="p1"><img alt="" src="images/01.jpg" ></p>',
+
+    '<p class="title">漫步操场</p>' +
+    '<p class="p1"><img alt="" src="images/02.jpg" ></p>',
+
+    '<p class="title">浪漫金鹿园</p>' +
+    '<p class="p1"><img alt="" src="images/03.jpg" ></p>',
+
+    '<p class="title">淡淡的生活</p>' +
+'<p class="p1"><img alt="" src="images/04.jpg" ></p>',
+
+    '<p class="title">温馨跨年</p>' +
+    '<p class="p1"><img alt="" src="images/051.jpg" ></p>',
+
+    '<p class="title">森林公园</p>' +
+    '<p class="p1"><img alt="" src="images/052.jpg" ></p>',
+
+    '<p class="title">暖暖午后</p>' +
+    '<p class="p1"><img alt="" src="images/053.jpg" ></p>',
+
+    '<p class="title">静静的岁月</p>' +
+    '<p class="p1"><img alt="" src="images/06.jpg" ></p>',
+
+    '<p class="title">归家的日子</p>' +
+    '<p class="p1"><img alt="" src="images/071.jpg" ></p>',
+
+
+    '<p class="title">离别</p>' +
+    '<p class="p1"><img alt="" src="images/072.jpg" ></p>',
+
+    '<p class="title">广州游玩</p>' +
+    '<p class="p1"><img alt="" src="images/081.jpg" ></p>',
+
+    '<p class="title">欢乐长隆</p>' +
+    '<p class="p1"><img alt="" src="images/082.jpg" ></p>',
+
+    '<p class="title">木马公举</p>' +
+    '<p class="p1"><img alt="" src="images/083.jpg" ></p>',
+
+
+
+//最后一页
+'<div class="bookshow"><p class="first-words" style="text-align:center;font-size:18px;color:#f7679d">送给我最爱的大公举</p>' +
+'<p class="second-words" style="text-align:center;font-size:15px;color:#f7679d">祝大公举情人节快乐，天天开心，不要有烦恼！</p>' +
+'<p class="third-words" style="text-align:right;font-size:15px;color:#f7679d">--专属你的小王子献上</p>' +
+'<p class="five-words" style="margin-left: 50px;"><img alt="" src="images/001.png" ></p></div>'];
+
+
+var start_page = 0;
